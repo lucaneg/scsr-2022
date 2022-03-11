5436b7301d99bd81ca1107b8c7a642369df9d944
MarcoTubia <867850@stud.unive.it>
2022-03-11 13:56:55 +0100

    adding some code

--- src/main/java/it/unive/scsr/AvailableExpressions.java
+++ src/main/java/it/unive/scsr/AvailableExpressions.java
@@ -1,29 +1,106 @@
 package it.unive.scsr;
 
-public class AvailableExpressions {
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
+import it.unive.lisa.analysis.ScopeToken;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowElement;
+import it.unive.lisa.analysis.dataflow.PossibleForwardDataflowDomain;
+import it.unive.lisa.analysis.representation.DomainRepresentation;
+import it.unive.lisa.analysis.representation.StringRepresentation;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.symbolic.value.ValueExpression;
 
-	// IMPLEMENTATION NOTE:
-	// the code below is outside of the scope of the course. You can uncomment it to get
-	// your code to compile. Beware that the code is written expecting that a field named 
-	// "expression" of type ValueExpression exists in this class: if you name it differently,
-	// change also the code below to make it work by just using the name of your choice instead
-	// of "expression". If you don't have a field of type ValueExpression in your solution,
-	// then you should make sure that what you are doing is correct :)
+public class AvailableExpressions 
+			 	implements DataflowElement<
+						PossibleForwardDataflowDomain<AvailableExpressions>,
+						AvailableExpressions>{
+	private final ValueExpression expression;
 	
-//	@Override
-//	public DomainRepresentation representation() {
-//		return new StringRepresentation(expression);
-//	}
-//
-//	@Override
-//	public AvailableExpressions pushScope(ScopeToken scope) throws SemanticException {
-//		return new AvailableExpressions((ValueExpression) expression.pushScope(scope));
-//	}
-//
-//	@Override
-//	public AvailableExpressions popScope(ScopeToken scope) throws SemanticException {
-//		return new AvailableExpressions((ValueExpression) expression.popScope(scope));
-//	}
+	public AvailableExpressions(ValueExpression expression) {
+		this.expression = expression;
+	}
+	
+	@Override
+	public Collection<Identifier> getInvolvedIdentifiers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<AvailableExpressions> gen(Identifier id, ValueExpression expression, ProgramPoint pp,
+			PossibleForwardDataflowDomain<AvailableExpressions> domain) throws SemanticException {
+		
+		Set<AvailableExpressions> result = new HashSet<>();
+		AvailableExpressions rd = new AvailableExpressions(expression);
+		result.add(rd);
+		return result;
+	}
+
+
+
+	@Override
+	public Collection<AvailableExpressions> gen(ValueExpression expression, ProgramPoint pp,
+			PossibleForwardDataflowDomain<AvailableExpressions> domain) throws SemanticException {
+		return new HashSet<>();
+	}
+	
+	@Override
+	public Collection<AvailableExpressions> kill(Identifier id, ValueExpression expression, ProgramPoint pp,
+			PossibleForwardDataflowDomain<AvailableExpressions> domain) throws SemanticException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Collection<AvailableExpressions> kill(ValueExpression expression, ProgramPoint pp,
+			PossibleForwardDataflowDomain<AvailableExpressions> domain) throws SemanticException {
+		return new HashSet<>();
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(expression);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AvailableExpressions other = (AvailableExpressions) obj;
+		return Objects.equals(expression, other.expression);
+	}
+	
+	//IMPLEMENTATION NOTE:
+		// the code below is outside of the scope of the course. You can uncomment it to get
+		// your code to compile. Beware that the code is written expecting that a field named 
+		// "expression" of type ValueExpression exists in this class: if you name it differently,
+		// change also the code below to make it work by just using the name of your choice instead
+		// of "expression". If you don't have a field of type ValueExpression in your solution,
+		// then you should make sure that what you are doing is correct :)
+	
+	@Override
+	public DomainRepresentation representation() {
+		return new StringRepresentation(expression);
+	}
+
+	@Override
+	public AvailableExpressions pushScope(ScopeToken scope) throws SemanticException {
+		return new AvailableExpressions((ValueExpression) expression.pushScope(scope));
+	}
+
+	@Override
+	public AvailableExpressions popScope(ScopeToken scope) throws SemanticException {
+		return new AvailableExpressions((ValueExpression) expression.popScope(scope));
+	}
 }
 
