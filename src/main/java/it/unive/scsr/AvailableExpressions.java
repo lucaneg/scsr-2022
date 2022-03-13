772783fe6d7b863416b33215e973dc9e03f0c92f
Mario <ale@MBP-di-Ale-16.station>
2022-03-13 22:20:49 +0100

    AvailableExpressions class definition

--- src/main/java/it/unive/scsr/AvailableExpressions.java
+++ src/main/java/it/unive/scsr/AvailableExpressions.java
@@ -1,7 +1,25 @@
 package it.unive.scsr;
 
-public class AvailableExpressions {
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
+import it.unive.lisa.analysis.ScopeToken;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowElement;
+import it.unive.lisa.analysis.dataflow.DefiniteForwardDataflowDomain;
+import it.unive.lisa.analysis.dataflow.PossibleForwardDataflowDomain;
+import it.unive.lisa.analysis.representation.DomainRepresentation;
+import it.unive.lisa.analysis.representation.PairRepresentation;
+import it.unive.lisa.analysis.representation.StringRepresentation;
+import it.unive.lisa.program.cfg.CodeLocation;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.symbolic.value.OutOfScopeIdentifier;
+import it.unive.lisa.symbolic.value.ValueExpression;
+
+public class AvailableExpressions implements DataflowElement<DefiniteForwardDataflowDomain<AvailableExpressions>, AvailableExpressions> {
 
 	// IMPLEMENTATION NOTE:
 	// the code below is outside of the scope of the course. You can uncomment it to get
@@ -25,5 +43,85 @@ public class AvailableExpressions {
 //	public AvailableExpressions popScope(ScopeToken scope) throws SemanticException {
 //		return new AvailableExpressions((ValueExpression) expression.popScope(scope));
 //	}
+	
+	private final ValueExpression expression;
+
+    public AvailableExpressions() {
+		this(null);
+	}
+
+	public AvailableExpressions(ValueExpression expression) {
+		this.expression = expression;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(expression);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AvailableExpressions other = (AvailableExpressions) obj;
+		return Objects.equals(expression, other.expression);
+	}
+
+	@Override
+	public Collection<Identifier> getInvolvedIdentifiers() {
+		return null;
+	}
+
+    @Override
+	public Collection<AvailableExpressions> gen(Identifier id, ValueExpression expression, ProgramPoint pp,
+			DefiniteForwardDataflowDomain<AvailableExpressions> domain) throws SemanticException {
+		Set<AvailableExpressions> result = new HashSet<>();
+		AvailableExpressions ae = new AvailableExpressions(expression);
+		result.add(ae);
+		return result;
+	}
+
+	@Override
+	public Collection<AvailableExpressions> gen(ValueExpression expression, ProgramPoint pp,
+			DefiniteForwardDataflowDomain<AvailableExpressions> domain) throws SemanticException {
+		return new HashSet<>();
+	}
+
+	@Override
+	public Collection<AvailableExpressions> kill(Identifier id, ValueExpression expression, ProgramPoint pp,
+			DefiniteForwardDataflowDomain<AvailableExpressions> domain) throws SemanticException {
+		Set<AvailableExpressions> result = new HashSet<>();
+
+		for (AvailableExpressions ae : domain.getDataflowElements())
+			if (ae.expression.equals(expression))
+				result.add(ae);
+
+		return result;
+	}
+
+	@Override
+	public Collection<AvailableExpressions> kill(ValueExpression expression, ProgramPoint pp,
+			DefiniteForwardDataflowDomain<AvailableExpressions> domain) throws SemanticException {
+		return new HashSet<>();
+	}
+
+    @Override
+    public DomainRepresentation representation() {
+        return new StringRepresentation(expression);
+    }
+
+    @Override
+    public AvailableExpressions pushScope(ScopeToken scope) throws SemanticException {
+        return new AvailableExpressions((ValueExpression) expression.pushScope(scope));
+    }
+
+    @Override
+    public AvailableExpressions popScope(ScopeToken scope) throws SemanticException {
+        return new AvailableExpressions((ValueExpression) expression.popScope(scope));
+    }
 }
 
