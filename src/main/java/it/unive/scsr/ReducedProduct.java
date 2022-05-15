6f022e5cf5b88a83a5d434d391a21779dc8d2792
cappellinsamuele <40738231+cappellinsamuele@users.noreply.github.com>
2022-05-15 18:21:08 +0200

    eval implementation attempts, unit test class

--- src/main/java/it/unive/scsr/ReducedProduct.java
+++ src/main/java/it/unive/scsr/ReducedProduct.java
@@ -1,23 +1,25 @@
 package it.unive.scsr;
 
-
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+import it.unive.lisa.analysis.nonrelational.value.ValueEnvironment;
 import it.unive.lisa.analysis.numeric.Parity;
 import it.unive.lisa.analysis.representation.DomainRepresentation;
 import it.unive.lisa.analysis.representation.PairRepresentation;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.ValueExpression;
 
 public class ReducedProduct extends BaseNonRelationalValueDomain<ReducedProduct> {
     
     // Left field is related to the abstract domain: "Extended sign"
-    public ExtSignDomainSolution left;
+    public ExtSignDomainSolution sign;
 
     // Right field is reletad to the abstract domain: "Parity"
-    public Parity right;
+    public Parity parity;
 
-    public ReducedProduct(ExtSignDomainSolution left, Parity right) {
-        this.left = left;
-        this.right = right;
+    public ReducedProduct(ExtSignDomainSolution sign, Parity parity) {
+        this.sign = sign;
+        this.parity = parity;
     }
 
     public ReducedProduct() {
@@ -27,37 +29,37 @@ public class ReducedProduct extends BaseNonRelationalValueDomain<ReducedProduct>
     @Override
     public DomainRepresentation representation() {
         // Build a representation which contains left (ExtSign domain) and right (Parity domain)
-        return new PairRepresentation(left.representation(), right.representation());
+        return new PairRepresentation(sign.representation(), parity.representation());
     }
 
     @Override
     public ReducedProduct top() {
         // Create a new element in the domain with top pair
-        return new ReducedProduct(left.top(), right.top());
+        return new ReducedProduct(sign.top(), parity.top());
     }
 
     @Override
     public ReducedProduct bottom() {
         // Create a new element in the domain with bottom pair
-        return new ReducedProduct(left.bottom(), right.bottom());
+        return new ReducedProduct(sign.bottom(), parity.bottom());
     }
 
     @Override
     public boolean isBottom() {
         // Element in the domain is bottom if both left and right are bottom
-        return this.left.isBottom() && this.right.isBottom();
+        return this.sign.isBottom() && this.parity.isBottom();
     }
 
     @Override
     public boolean isTop() {
         // Element in the domain is top if both left and right are top
-        return this.left.isTop() && this.right.isTop();
+        return this.sign.isTop() && this.parity.isTop();
     }
 
     @Override
     protected ReducedProduct lubAux(ReducedProduct other) throws SemanticException {
         // Create new element in the domain where each element of the pair is a comparison between this and other element of the domain
-        return new ReducedProduct(this.left.lub(other.left), this.right.lub(other.right));
+        return new ReducedProduct(this.sign.lub(other.sign), this.parity.lub(other.parity));
     }
 
     @Override
@@ -69,7 +71,7 @@ public class ReducedProduct extends BaseNonRelationalValueDomain<ReducedProduct>
     @Override
     protected boolean lessOrEqualAux(ReducedProduct other) throws SemanticException {
         // Returns true if and only if this lattice element <= than given one.
-        return this.left.lessOrEqual(other.left) && this.right.lessOrEqual(other.right);
+        return this.sign.lessOrEqual(other.sign) && this.parity.lessOrEqual(other.parity);
     }
 
     @Override
@@ -87,16 +89,16 @@ public class ReducedProduct extends BaseNonRelationalValueDomain<ReducedProduct>
         ReducedProduct other = (ReducedProduct) obj;
 
         // Check if left field of this and other are not equal
-        if (this.left == null && other.left != null) {
+        if (this.sign == null && other.sign != null) {
             return false;
-        } else if (!this.left.equals(other.left)) {
+        } else if (!this.sign.equals(other.sign)) {
             return false;
         }
 
         // Check if right field of this and other are not equal
-        if (this.right == null && other.right != null) {
+        if (this.parity == null && other.parity != null) {
             return false;
-        } else if(!this.right.equals(other.right)) {
+        } else if(!this.parity.equals(other.parity)) {
             return false;
         }
         
@@ -111,12 +113,25 @@ public class ReducedProduct extends BaseNonRelationalValueDomain<ReducedProduct>
         int res = 1;
 
         // Calculate result using hash code of left (ExtSign domain)
-        res = prime * res + ((this.left == null) ? 0 : this.left.hashCode());
+        res = prime * res + ((this.sign == null) ? 0 : this.sign.hashCode());
 
         // Calculate result using hash code of right (Parity domain)
-        res = prime * res + ((this.right == null) ? 0 : this.right.hashCode());
+        res = prime * res + ((this.parity == null) ? 0 : this.parity.hashCode());
 
         return res;
     }
 
+    @Override
+    public ReducedProduct eval(ValueExpression expression, ValueEnvironment<ReducedProduct> environment,
+            ProgramPoint pp) throws SemanticException {
+        // TODO Auto-generated method stub
+        
+        //System.out.println("eval");
+
+        ValueEnvironment<Parity> vp = new ValueEnvironment<Parity>(environment.lattice.parity);
+        ValueEnvironment<ExtSignDomainSolution> vs = new ValueEnvironment<ExtSignDomainSolution>(environment.lattice.sign);
+        Parity p = this.parity.eval(expression, vp, pp);
+        ExtSignDomainSolution s = this.sign.eval(expression, vs, pp);
+        return new ReducedProduct(s, p);
+    }
 }
